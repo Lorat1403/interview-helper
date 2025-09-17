a8ca0dcbad992856e8c86698c16b1f425aca67f5
Lorat1403 <lorat1403@gmail.com>
2025-09-17 17:23:18 +0300

    create components

---
+++ src/app/components/delete-confirm-modal/delete-confirm-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-confirm-modal',
+  imports: [],
+  templateUrl: './delete-confirm-modal.html',
+  styleUrl: './delete-confirm-modal.scss'
+})
+export class DeleteConfirmModal {
+
+}
