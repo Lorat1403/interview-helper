8b1ec38388372b094ac50fab70499eb14d2839b8
Lorat1403 <lorat1403@gmail.com>
2025-09-19 10:48:48 +0300

    add left-side-menu, preparation

--- src/app/components/left-side-menu/left-side-menu.ts
+++ src/app/components/left-side-menu/left-side-menu.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+
 
 @Component({
   selector: 'app-left-side-menu',
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './left-side-menu.html',
   styleUrl: './left-side-menu.scss'
 })
