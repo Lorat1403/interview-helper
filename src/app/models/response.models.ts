900c243defac9dd70b237acfa12e999b94f561f5
Lorat1403 <lorat1403@gmail.com>
2025-09-21 16:24:47 +0300

    created json-server and edited package.json to run  both frontend and backend concurrently

--- src/app/models/response.models.ts
+++ src/app/models/response.models.ts
@@ -1,3 +1,5 @@
+import { QuestionItem } from "../components/category/category.config";
+
 export interface Responce<T>{
   data: T;
   errror?: string;
@@ -6,5 +8,27 @@ export interface Responce<T>{
 export interface ResponceArray<T>{
   data: T[];
   error?: string;
+}
+
+export interface UserRegisterResponse {
+  accessToken: string;
+  user: User;
+}
+
+export interface UserLoginResponse extends UserRegisterResponse {}
+
+export interface CategoryResponse {
+  id: number;
+  categoryId: number;
+  name: string;
+  questions: QuestionItem[];
+}
+
+export interface User {
+  id: number;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
 }
   
